df847ffa23e4174cdf151c40a0920476c0f76cd1
Alexsander Akers <me@a2.io>
2018-03-14 22:06:01 +0100

    Change link color to teal

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,9 @@ import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 
 Wordpress2016.overrideThemeStyles = () => ({
+  'a': {
+    color: '#00a88f',
+  },
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
