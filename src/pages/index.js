af9ed45fd05e0e7fa242468ee42dd79ce21f2de4
Alexsander Akers <me@a2.io>
2018-05-03 16:59:23 +0200

    Filter homepage markdown to blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fields: { slug: { regex: "^/blog/" } } }
+    ) {
       edges {
         node {
           excerpt
