1218c3625751b5438ce8560c85ae69ffd375fdc4
Alexsander Akers <me@a2.io>
2018-05-04 11:50:58 +0200

    Only show blog posts on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -41,7 +41,8 @@ export const pageQuery = graphql`
     }
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
-      limit: 3
+      limit: 3,
+      filter: { fields: { slug: { regex: "^/blog/" } } }
     ) {
       edges {
         node {
